fba731dccfb7fbab3a5456265bb0e4ac61f9cacc
sksksky <spbarte22@src.pshs.edu.ph>
2025-03-03 19:44:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 function verifyMessage(){
   let message = prompt("Enter the message to be verified: ");
 
-  if (message.toUppercase().indexOf("FR") >= 0 ){
-    result = true;
-    return document.getElementById("result").innerHTML = "Your message " ${message} " is legitimate!";
-  } else if (message.toUppercase().indexOf("AI") >= 0 ){
-    result = false;
-    return document.getElementById("result").innerHTML = "Your message " ${message} " is fake!";
-  } else if (message.toUppercase().indexOf("FR") >= 0 && message.toUppercase().indexOf("AI")){
-    result = true;
-    return document.getElementById("result").innerHTML = "Your message " ${message} " is legitimate!";
-  } else if (message.indexOf("Ai") >= 0 || message.indexOf("aI" >= 0 ){
-    result = true;
-    return document.getElementById("result").innerHTML = "Your message " ${message} " is fake!";
+  if (message.toUpperCase().indexOf("FR") >= 0 ){
+    document.getElementById("result").innerHTML = `Your message "${message}" is legitimate!`;
+  } else if (message.toUpperCase().indexOf("AI") >= 0 ){
+    document.getElementById("result").innerHTML = `Your message "${message}" is fake!`;
+  } else if (message.toUpperCase().indexOf("FR") >= 0 && message.toUpperCase().indexOf("AI") >= 0){
+    document.getElementById("result").innerHTML = `Your message "${message}" is legitimate!`;
+  } else if (message.indexOf("Ai") >= 0 || message.indexOf("aI") >= 0 ){
+    document.getElementById("result").innerHTML = `Your message "${message}" is fake!`;
   } else {
-    result = false;
-    return document.getElementById("result").innerHTML = "Your message " ${message} " is yet to be encoded.";
+    document.getElementById("result").innerHTML = `Your message "${message}" is yet to be encoded.`;
   }
 }

c5c620123a7cd8cf06e73f215d4185b2205789c5
sksksky <spbarte22@src.pshs.edu.ph>
2025-03-03 09:19:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ function verifyMessage(){
 
   if (message.toUppercase().indexOf("FR") >= 0 ){
     result = true;
-    return document.getElementById("result").innerHTML = Your message " ${message} " is legitimate!;
+    return document.getElementById("result").innerHTML = "Your message " ${message} " is legitimate!";
   } else if (message.toUppercase().indexOf("AI") >= 0 ){
     result = false;
-    return document.getElementById("result").innerHTML = Your message " ${message} " is fake!;
+    return document.getElementById("result").innerHTML = "Your message " ${message} " is fake!";
   } else if (message.toUppercase().indexOf("FR") >= 0 && message.toUppercase().indexOf("AI")){
     result = true;
-    return document.getElementById("result").innerHTML = Your message " ${message} " is legitimate!;
+    return document.getElementById("result").innerHTML = "Your message " ${message} " is legitimate!";
   } else if (message.indexOf("Ai") >= 0 || message.indexOf("aI" >= 0 ){
     result = true;
-    return document.getElementById("result").innerHTML = Your message " ${message} " is fake!;
+    return document.getElementById("result").innerHTML = "Your message " ${message} " is fake!";
   } else {
     result = false;
-    return document.getElementById("result").innerHTML = Your message " ${message} " is yet to be encoded.;
+    return document.getElementById("result").innerHTML = "Your message " ${message} " is yet to be encoded.";
   }
 }
